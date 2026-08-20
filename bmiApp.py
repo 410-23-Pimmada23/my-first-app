5fe20e2c5286494251f88d2a54cb7042a9d20cea
410-23-Pimmada23 <sophia040154@gmail.com>
2026-08-20 10:27:58 +0700

    Update bmiApp.py

--- bmiApp.py
+++ bmiApp.py
@@ -6,13 +6,14 @@ st.write("กรอกข้อมูลน้ำหนักและส่ว�
 
 #ส่วนที่2 สร้างช่องรับค่าน้ำหนัก และ ส่วนสูง
 wight = st.number_input("กรอกน้ำหนักของคุณ (กิโลกรัม):", min_value=1.0, value=1.0)
-height_cm = st.number_input("กรอกน้ส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
+height_cm = st.number_input("กรอกส่วนสูงของคุณ (เซนติเมตร):", min_value=1.0, value=1.0)
 
 #ส่วนที่3 สร้างปุ่มกดคำนวณ
 if st.button("คำนวณค่า BMI 🚨"):
    # แปลงส่วนสูงจาก cm เป็น เมตร แล้วคำนวน BMI
    height_m = height_cm / 100
    bmi = weight / (height_m ** 2)
+   st.write("ค่า bmi =" , bmi)
    
    st.write("---")
    st.header(f"ค่า BMI ของคุณคือ: **{bmi:.2f}**")
